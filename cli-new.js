52cc44ef162e7fa0f61115bd64a286381e58f92e
Pelle Wessman <pelle@kodfabrik.se>
2019-08-02 16:06:01 +0200

    Even more WIP

--- cli-new.js
+++ cli-new.js
@@ -5,19 +5,61 @@
 // @ts-check
 /// <reference types="node" />
 
+const { resolve: pathResolve } = require('path');
+
+const cosmiconfig = require('cosmiconfig');
+const debug = require('debug')('envsync');
+const meow = require('meow');
+
 const {
   initEnvSync,
   doEnvSync
 } = require('.');
 
-initEnvSync({
-  baseDir: __dirname,
-  plugins: ['./lib/plugins/travis']
-}).then(async (result) => {
+const cli = meow(`
+    Usage
+      $ envsync <path>
+
+    Options
+      --dry-run,            Runs everything as normal, but doesn't do any changes
+
+    Examples
+      $ envsync .
+`, {
+  flags: {
+    'dry-run': {
+      type: 'boolean'
+    }
+  }
+});
+
+// const {
+//   dryRun
+// } = cli.flags;
+
+const baseDir = cli.input[0]
+  ? pathResolve(process.cwd(), cli.input[0])
+  : process.cwd();
+
+debug('Looking for config in: %s', baseDir);
+
+const explorer = cosmiconfig('envsync', { stopDir: baseDir });
+
+explorer.search(baseDir).then(async foundConfig => {
+  if (!foundConfig) {
+    throw new Error('Couldn\'t find a config file');
+  }
+
+  const config = Object.assign({}, foundConfig.config, { baseDir });
+
+  debug('Resolved config: %O', config);
+
   const {
     targets,
     secrets
-  } = result;
+  } = await initEnvSync(config);
+
+  debug('Sync setup initiated');
 
   const secretNames = Object.keys(secrets);
 
@@ -26,13 +68,19 @@ initEnvSync({
     console.log('Lets resolve some secrets!');
   }
 
+  debug('Secrets resolved');
+
   const resolvedEnv = {};
 
   for (const target of targets) {
+    debug('Syncing "%s" target', target.name || 'unnamed');
+
     doEnvSync(target, resolvedEnv, { statusCallback: (...params) => { console.log('😁', ...params); } });
   }
+
+  debug('Syncing complete');
 }).catch(err => {
   // eslint-disable-next-line no-console
-  console.error(err.stack);
+  console.error('An error occured:', err.stack);
   process.exit(1);
 });
