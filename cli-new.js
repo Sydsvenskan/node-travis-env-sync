7213ae9b420d94b5d7abbf44ff4f2ac2a11adab4
Pelle Wessman <pelle@kodfabrik.se>
2019-08-07 16:14:40 +0200

    WIP

--- cli-new.js
+++ cli-new.js
@@ -5,6 +5,8 @@
 // @ts-check
 /// <reference types="node" />
 
+/** @typedef {import('./lib/types').EnvData} EnvData */
+
 const { resolve: pathResolve } = require('path');
 
 const cosmiconfig = require('cosmiconfig');
@@ -55,27 +57,61 @@ explorer.search(baseDir).then(async foundConfig => {
   debug('Resolved config: %O', config);
 
   const {
+    base: {
+      plugins
+    },
     targets,
     secrets
   } = await initEnvSync(config);
 
   debug('Sync setup initiated');
 
+  const secretStores = plugins.filter(plugin => !!plugin.secretStore);
   const secretNames = Object.keys(secrets);
+  const resolvedSecrets = {};
 
-  if (secretNames) {
-    // FIXME: Resolve some secrets!
-    console.log('Lets resolve some secrets!');
+  if (secretNames.length) {
+    debug('Resolving %d secrets', secretNames.length);
+
+    // TODO: Ask for description of secret
+    // TODO: Ask for secret value
+    // TODO: Validate secret value
+    // TODO: Add a keychain store
+    for (const secret of secretNames) {
+      let result;
+
+      for (const plugin of secretStores) {
+        result = plugin.secretStore.get(secret);
+
+        if (result !== undefined) {
+          debug('Found secret: %s', secret);
+          resolvedSecrets[secret] = result;
+          break;
+        }
+      }
+
+      if (!result) {
+        debug('Did not find secret: %s', secret);
+      }
+    }
+
+    debug('%d secrets resolved', Object.keys(resolvedSecrets).length);
+
+    // FIXME: For any missing secret: Ask for the user to provide it and then store it if possible (unless an option specified that we should ask no such questions and/or store no such answers)
+
+    // FIXME: If any of the unresolved secrets were required secrets: Fail in runEnvSync (it should be done for all implementations, not just the CLI implementation)
+    // TODO: Maybe implement the secret store outside of the CLI? Useful elsewhere?
   }
 
-  debug('Secrets resolved');
-
+  /** @type {EnvData} */
   const resolvedEnv = {};
 
   for (const target of targets) {
-    debug('Syncing "%s" target', target.name || 'unnamed');
-
-    doEnvSync(target, resolvedEnv, { statusCallback: (...params) => { console.log('😁', ...params); } });
+    await doEnvSync(target, resolvedEnv, {
+      statusCallback: (step, message) => {
+        debug('Syncing %s target: %s', target.name || 'unnamed', message);
+      }
+    });
   }
 
   debug('Syncing complete');
