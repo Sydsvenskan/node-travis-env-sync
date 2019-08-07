8c3121648c7f7bc66a39375dbd040f2bbcb1e7ef
Pelle Wessman <pelle@kodfabrik.se>
2019-08-07 16:42:58 +0200

    WIP – now collects secrets

--- cli-new.js
+++ cli-new.js
@@ -11,6 +11,7 @@ const { resolve: pathResolve } = require('path');
 
 const cosmiconfig = require('cosmiconfig');
 const debug = require('debug')('envsync');
+const inquirer = require('inquirer');
 const meow = require('meow');
 
 const {
@@ -66,11 +67,15 @@ explorer.search(baseDir).then(async foundConfig => {
 
   debug('Sync setup initiated');
 
-  const secretStores = plugins.filter(plugin => !!plugin.secretStore);
+  /** @type {EnvData} */
   const secretNames = Object.keys(secrets);
   const resolvedSecrets = {};
+  const resolvedEnv = {};
 
   if (secretNames.length) {
+    const secretStores = plugins.filter(plugin => !!plugin.secretStore);
+    const secretPrompts = [];
+
     debug('Resolving %d secrets', secretNames.length);
 
     // TODO: Ask for description of secret
@@ -80,8 +85,8 @@ explorer.search(baseDir).then(async foundConfig => {
     for (const secret of secretNames) {
       let result;
 
-      for (const plugin of secretStores) {
-        result = plugin.secretStore.get(secret);
+      for (const store of secretStores) {
+        result = store.secretStore.get(secret);
 
         if (result !== undefined) {
           debug('Found secret: %s', secret);
@@ -92,19 +97,39 @@ explorer.search(baseDir).then(async foundConfig => {
 
       if (!result) {
         debug('Did not find secret: %s', secret);
+        secretPrompts.push({
+          type: 'password',
+          message: secrets[secret].provider.secretProviders[secret],
+          name: secret
+        });
       }
     }
 
     debug('%d secrets resolved', Object.keys(resolvedSecrets).length);
 
+    if (secretPrompts.length) {
+      debug('Asking user about %d secrets', Object.keys(secretPrompts).length);
+
+      const userSecrets = await inquirer.prompt(secretPrompts);
+
+      for (const secretName in userSecrets) {
+        if (!userSecrets[secretName]) {
+          console.log('😗', secretName);
+          delete userSecrets[secretName];
+        }
+      }
+
+      Object.assign(resolvedSecrets, userSecrets);
+    }
+
+    debug('Used secrets: %s', Object.keys(resolvedSecrets).join(', '));
     // FIXME: For any missing secret: Ask for the user to provide it and then store it if possible (unless an option specified that we should ask no such questions and/or store no such answers)
 
     // FIXME: If any of the unresolved secrets were required secrets: Fail in runEnvSync (it should be done for all implementations, not just the CLI implementation)
     // TODO: Maybe implement the secret store outside of the CLI? Useful elsewhere?
-  }
 
-  /** @type {EnvData} */
-  const resolvedEnv = {};
+    resolvedEnv.secrets = resolvedSecrets;
+  }
 
   for (const target of targets) {
     await doEnvSync(target, resolvedEnv, {
